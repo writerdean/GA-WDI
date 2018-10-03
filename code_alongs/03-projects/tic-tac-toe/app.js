a0e661e614618e1b4590c08325d83f84ac59ccac
Tracy Dean <tracyblackadder@yahoo.com>
2018-10-03 20:20:19 +1000

    tic tac toe working

--- code_alongs/03-projects/tic-tac-toe/app.js
+++ code_alongs/03-projects/tic-tac-toe/app.js
@@ -10,6 +10,8 @@ var activeText = document.querySelector('.active') // for styling
 var inActiveText = document.querySelector('.inactive') // for styling
 var playerOneClicks = [];
 var playerTwoClicks = [];
+var winningLine = [];
+var clickedSquareId;
 
 var rowOne = ['R1C1', 'R1C2', 'R1C3']
 var rowTwo = ['R2C1', 'R2C2', 'R2C3']
@@ -18,8 +20,69 @@ var colOne = ['R1C1', 'R2C1', 'R3C1']
 var colTwo = ['R1C2', 'R2C2', 'R3C2']
 var colThree = ['R1C3', 'R2C3', 'R3C3']
 var diagOne = ['R1C1', 'R2C2', 'R3C3']
-var diagTwo = ['R1C2', 'R2C2', 'R3C1']
+var diagTwo = ['R1C3', 'R2C2', 'R3C1']
 
+function test() {
+    for (var i = 0; i < playerOneClicks.length; i++) {
+        if (rowOne.indexOf(playerOneClicks[i]) === -1) {
+            console.log('none found in test function')
+        }
+        console.log(playerOneClicks[i] + rowOne.indexOf)
+    }
+}
+
+function checkForMatch (playerArr, winArr) {
+    console.log(playerArr)
+    console.log(winArr)
+    for(var i = 0; i < winArr.length; i++) {
+        console.log(winArr[i])
+
+        if (playerArr.indexOf(winArr[i]) === -1){
+            console.log('is false')
+            return false
+        }  
+    }
+    console.log('is true')
+    return true    
+}
+
+function checkIfPlayerWins (person) {
+    
+    if (checkForMatch(person, rowOne)) {
+        return true
+    }
+    if (checkForMatch(person, rowTwo)) {
+        return true
+    }
+    if (checkForMatch(person, rowThree)) {
+        return true
+    }
+    if (checkForMatch(person, colOne)) {
+        return true
+    }
+    if (checkForMatch(person, colTwo)) {
+        return true
+    }
+    if (checkForMatch(person, colThree)) {
+        return true
+    }
+    if (checkForMatch(person, diagOne)) {
+        return true
+    }
+    if (checkForMatch(person, diagTwo)) {
+        return true
+    }
+    return false
+}
+
+function checkForWinner () {
+    if(checkIfPlayerWins(playerOneClicks)) {
+        console.log('Player One wins!!!!!!')
+    } 
+    if(checkIfPlayerWins(playerTwoClicks)) {
+        console.log('Player Two wins.')
+    }
+}
 
 // this changes the turn to the next person
 var nextTurn = function() {
@@ -46,38 +109,33 @@ var toggleActiveClass = function() {
 }
 
 
+// gets the id of the clicked square
+function replyClick(clicked_id) {
+    clickedSquareId = clicked_id;
+    console.log(clickedSquareId)
+}
+
+
 // this function will enter a token in the square, and indicate that the square has now been clicked
 var chooseSquare = function(event) {
+
     if (event.target.classList.contains('clicked')) {
         alert('Please choose another square')
     } else {
         if(turnIndicator === 'playerOne') {
             event.target.textContent = playerOneToken;
             event.target.classList.add('P1C')
-            playerOneClicks.push(this.id)
-            
+            playerOneClicks.push(clickedSquareId)
             
         } else {
             event.target.textContent = playerTwoToken;
             event.target.classList.add('P2C')
+            playerTwoClicks.push(clickedSquareId)
         }
         event.target.classList.add('clicked')
         nextTurn();
     }
 }
 
-var checkForWinner = function() {
-    if(turnCounter>4) {
-        
-
-    } else {
-        return
-    }
-}
-
-function replyClick(clicked_id) {
-    console.log(clicked_id)
-
-}
 
 board.addEventListener('click', chooseSquare)
